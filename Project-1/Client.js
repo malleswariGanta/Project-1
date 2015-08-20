3775bf3f6acc3d1fd15aaf93034c5ed3ca4e0a30
malleswari.ganta <malleswari@tvarana.com>
2015-08-20 23:02:52 +0530

    Initial Commit

---
+++ Project-1/Client.js
@@ -0,0 +1,18 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       20 Jun 2014     MALLESWARI
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType 
+ * 
+ * @param {String} type Access mode: create, copy, edit
+ * @returns {Void}
+ */
+function sample() {
+	
+}
---
+++ Project-1/Field.js
@@ -0,0 +1,27 @@
+
+function clientPageInit(type) {
+	
+	//set default value to the custom filed
+	var recId = nlapiGetRecordId();
+	var recType = nlapiGetRecordType();
+	var record = nlapiLoadRecord(recType,recId);
+		
+	var context = nlapiGetContext();
+	var userName = context.getUser();        // entity id of the current user 
+	var userRole = context.getRole();        // id of the current user's role 
+	var userDept = context.getDepartment();  // id of the current user's department 
+	var userLoc = context.getLocation();     // id of the current user's location 
+
+	//  Display information
+	alert("Current User Information" + "\n\n" +
+		 "Name: " + userName + "\n" +
+		 "Role: " + userRole + "\n" +
+		 "Dept: " + userDept + "\n" +
+		 "Loc: " + userLoc
+		 );
+   var data = "Name: " + userName + "\n" + "Role: " + userRole + "\n" +  "Dept: " + userDept + "\n" +  "Loc: " + userLoc;
+	
+	record.setFieldValue('custbody_g_field',data);
+	m=nlapiDisableField('custbody_g_field',true);
+	nlapiSubmitRecord(record);
+}
